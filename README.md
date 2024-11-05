5c774b60cd87d2ae0a7f9a42f64774e16b9775db
Tristan Hebert <tristanhebrt@gmail.com>
2024-11-04 21:26:50 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# kamerakidstudios2
+ 
---
+++ src/Home/Home.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './Home.css'
+
+function Home() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Home

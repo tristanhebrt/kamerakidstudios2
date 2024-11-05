4ad950fc418b20c105e238ac7be11ab8a7b01925
Tristan Hebert <tristanhebrt@gmail.com>
2024-11-05 17:48:03 -0500

    Logo Update
    Changed the logo for a new cropped SVG.

--- src/components/HomeLogo/SVGLogo.jsx
+++ src/components/HomeLogo/SVGLogo.jsx
@@ -1,11 +1,32 @@
 import styles from './HomeLogo.module.css';
-
+import { motion } from 'framer-motion';
 import logo from '/src/assets/kamerakidstudios-logo.svg';
+import { useState } from 'react';
 
 function SVGLogo() {
+    const [isSpinning, setIsSpinning] = useState(false);
+
+    const handleClick = () => {
+        // Start the spinning animation
+        setIsSpinning(true);
+
+        // Stop the spinning after the animation completes
+        setTimeout(() => {
+            setIsSpinning(false);
+        }, 500); // Match this duration to the animation duration
+    };
+
     return (
-        <img src={logo} alt="Kamera Kid Studios Logo" width={1000} height={1000} />
-    )
+        <motion.img 
+            src={logo} 
+            alt="Kamera Kid Studios Logo" 
+            width={1000} 
+            height={'auto'} 
+            onClick={handleClick} // Handle the click
+            animate={{ rotate: isSpinning ? 720 : 0 }} // Rotate based on state
+            transition={{ duration: 0.5 }} // Duration for the rotation effect
+        />
+    );
 }
 
 export default SVGLogo;

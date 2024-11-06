a074cbd445fd14af9fc4de08a2b9fac2a0ef4070
Tristan Hebert <tristanhebrt@gmail.com>
2024-11-05 20:56:24 -0500

    Photo Gallery Update
    Pre picture commit

--- src/Gallery/Gallery.jsx
+++ src/Gallery/Gallery.jsx
@@ -1,10 +1,12 @@
+
+import styles from './Gallery.module.css'
 import PhotoGallery from '../components/PhotoGallery/PhotoGallery.jsx'
 
 function Gallery() {
     return (
-        <>
+        <div className={styles.galleryContainer}>
             <PhotoGallery/>
-        </>
+        </div>
     )
 }
 
--- src/components/PhotoGallery/PhotoGallery.jsx
+++ src/components/PhotoGallery/PhotoGallery.jsx
@@ -1,10 +1,24 @@
+import styles from './PhotoGallery.module.css';
 
 function PhotoGallery() {
     return (
-        <>
+        <div className={styles.photoGalleryContainer}>
             <h1>Photo Gallery</h1>
-            <img src="https://placehold.co/600x400"/>
-        </>
+            <h2>Photography</h2>
+            <div className={styles.photoContainer}>
+                <img src="https://placehold.co/150x150"/>
+                <img src="https://placehold.co/150x150"/>
+                <img src="https://placehold.co/150x150"/>
+                <img src="https://placehold.co/150x150"/>
+            </div>
+            <h2>Cover Art</h2>
+            <div className={styles.photoContainer}>
+                <img src="https://placehold.co/150x150"/>
+                <img src="https://placehold.co/150x150"/>
+                <img src="https://placehold.co/150x150"/>
+                <img src="https://placehold.co/150x150"/>
+            </div>
+        </div>
     )
 }
 

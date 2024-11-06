30f8df60def35720d238fad6f1ef668f4e6ddffa
Tristan Hebert <tristanhebrt@gmail.com>
2024-11-06 16:34:38 -0500

    Revert "Github Pages"
    This reverts commit 65755c1dc7633852e55cf576b665e61168904ac7.

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
-  base: "/kamerakidstudios/",
-
   build: {
     sourcemap: false, // Disable source maps
   },
